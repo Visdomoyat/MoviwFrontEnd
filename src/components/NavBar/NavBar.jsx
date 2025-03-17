d784440d378c17ad37045a18b43151e27edd182a
User Name <user@email.com>
2025-03-17 01:40:00 -0400

    Feat: navbar style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,33 +2,47 @@ import { useContext } from 'react';
 import { Link } from 'react-router';
 
 import { UserContext } from '../../contexts/UserContext';
+import styles from './NavBar.module.css'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import { useState } from 'react';
+import Logo from '../../assets/images/logo.png'
 
 const NavBar = () => {
   const { user, setUser } = useContext(UserContext);
-
+  const [isOpen, setIsOpen] = useState(false) // Toggle mobile menu
   const handleSignOut = () => {
     localStorage.removeItem('token');
     setUser(null);
+    setIsOpen(false);
   };
 
   return (
-    <nav>
-      {user ? (
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to ='/service'>Services</Link></li>
-          <li><Link to='/services'>Requested Services</Link></li>
-          <li><Link to ='/services/new'>Request Service</Link></li>
+    <nav className={styles.navbar}>
+      
+      <div className={styles.logoContainer}>
+        <Link to='/'>
+        <img src={Logo} alt="A dove Icon" className={styles.logo}/>
+        </Link>
+      </div> 
+      <div className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <FaTimes /> : <FaBars/>}
+      </div>
+      <ul className={`${styles.navLinks} ${isOpen ? styles.open : ''}`}>
+        <li><Link to='/' onClick={() => setIsOpen(false)}>Home</Link></li>
+        <li><Link to='/service' onClick={() => setIsOpen(false)}>Services</Link></li>
+       {user ? (
+        <>
+          <li><Link to='/services' onClick={() => setIsOpen(false)}>Requested Services</Link></li>
+          <li><Link to ='/services/new' onClick={() => setIsOpen(false)}>Request Service</Link></li>
           <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
-        </ul>
+        </>
       ) : (
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to ='/service'>Services</Link></li>
-          <li><Link to='/sign-in'>Sign In</Link></li>
-          <li><Link to='/sign-up'>Sign Up</Link></li>
-        </ul>
+        <>
+          <li><Link to='/sign-in' onClick={() => setIsOpen(false)}>Sign In</Link></li>
+          <li><Link to='/sign-up' onClick={() => setIsOpen(false)}>Sign Up</Link></li>
+        </>
       )}
+      </ul>
     </nav>
   );
 };
