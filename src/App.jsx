94b4c6076e7a9d73ae073b4c5bfdfd9280e06a94
User Name <user@email.com>
2025-03-16 20:57:04 -0400

    Feat: frontend navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useContext,useState, useEffect } from 'react';
 import * as serveService from './services/serveService';
-import { Routes, Route } from 'react-router';
+import { Routes, Route, useNavigate } from 'react-router';
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
@@ -8,6 +8,7 @@ import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
 import ServiceList from './components/ServiceList/ServiceLists';
 import ServiceDetails from './components/ServiceDetails/ServiceDetails';
+import ServiceForm from './components/ServiceForm/ServiceForm';
 
 import { UserContext} from './contexts/UserContext';
 
@@ -15,13 +16,35 @@ const App = () => {
   const { user } = useContext(UserContext);
 
   const [services, setServices] = useState([])
+
+  const navigate = useNavigate();
+
+  const handleAddService = async (serviceFormData) => {
+    try{
+      const newService = await serveService.create(serviceFormData)
+      console.log("New Service Created:", newService)
+      if (!newService) throw new Error("Service creation failed")
+      setServices([newService, ...services]);
+      navigate('/services')
+    }catch(err){
+      console.error("error adding service:", err)
+    }
+    
+  }
+
+  
   
   useEffect(() => {
     const fetchAllServices = async () => {
-      const servicesData = await serveService.index();
+      try{
+        const servicesData = await serveService.index();
       
-      console.log("Fetched services:", servicesData)
-      setServices(servicesData)
+        // console.log("Fetched services:", servicesData)
+        setServices(servicesData)
+      }catch(err){
+        console.error("Error fetching services:", err)
+      }
+      
     };
     if(user) fetchAllServices()
   }, [user])
@@ -33,8 +56,9 @@ const App = () => {
         {user ? (
           <>
           <Route path='/services' element={<ServiceList services={services} />} />
-          <Route path='/services/:serviceId' element={<ServiceDetails />} />
-          <Route path='/services/new' element={<h1>Request Service</h1>} />
+          <Route path='/services/:serviceId' element={<ServiceDetails services={services}/>} />
+          <Route path='/services/new' element={<ServiceForm handleAddService = {handleAddService} />} />
+          
           </>
         ) : (
           <>
